66e7ce0b53cc8084465a47cc1870358e87618764
eyal7775 <eyal.naftalovich@mdclone.com>
2022-07-19 14:53:05 +0300

    fix code to with

--- Dockerfile.dockerfile
+++ Dockerfile.dockerfile
@@ -3,7 +3,8 @@ COPY requirements.txt ./
 RUN pip install -r requirements.txt
 WORKDIR /usr/app/src
 COPY main.py ./
-CMD ["python", "./main.py", "logfilename.txt", "10.0.2.42"]
+RUN ["python", "./main.py", "logfilename.txt", "10.0.2.42"]
+CMD cat logfilename.txt
 
 # docker build -t test -f .\Dockerfile.dockerfile .
 # docker run test
--- main.py
+++ main.py
@@ -9,20 +9,17 @@ parser = argparse.ArgumentParser(description='Enter log path and ip')
 log_file = parser.add_argument("log_file", help="name of output file", type=str)
 ip_server = parser.add_argument("ip_server", help="server is found data to actions", type=str)
 args = parser.parse_args()
-if not os.path.exists(args.log_file):
-    file = open(args.log_file ,"w")
-else:
-    file = open(args.log_file, "a+")
-url = "http://" + args.ip_server + ":999/system_check"
-services = requests.get(url)
-strings = re.findall('<h1 style="color:[A-Za-z ]+">[A-Za-z ]+:</h1>' ,services.text)
-for string in strings:
-    date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    micro_service = re.search('>[A-Za-z ]+:<' ,string).group(0)[1:-2]
-    if "lightgreen" in string:
-        file.write(str(date) + "," + micro_service + ",Online\n")
-    else:
-        file.write(str(date) + "," + micro_service + ",Offline\n")
+with open(args.log_file, "a+") as file:
+    url = "http://" + args.ip_server + ":999/system_check"
+    services = requests.get(url)
+    strings = re.findall('<h1 style="color:[A-Za-z ]+">[A-Za-z ]+:</h1>' ,services.text)
+    for string in strings:
+        date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        micro_service = re.search('>[A-Za-z ]+:<' ,string).group(0)[1:-2]
+        if "lightgreen" in string:
+            file.write(str(date) + "," + micro_service + ",Online\n")
+        else:
+            file.write(str(date) + "," + micro_service + ",Offline\n")
 
 # create a new repository on the command line:
 # echo "# pythonProject1" >> README.md
